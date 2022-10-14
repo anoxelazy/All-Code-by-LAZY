3f7d2bfa7d6ad571b039c9bc1afa1a45296b1887
anoxelazy <106942679+anoxelazy@users.noreply.github.com>
2022-10-14 12:20:57 +0700

    Update hanoi.c

--- hanoi.c
+++ hanoi.c
@@ -5,17 +5,20 @@ void toh(int,char,char,char);
 int main()
 {
     int e;
-    printf("Enter the Nuber of Disks : ")
-    scanf("%d ,&e");
-    printf("\n")
-    toh(e'A' 'B' 'C');
+    printf("Enter the Nuber of Disks : ");
+    scanf("%d" ,&e);
+    printf("\n");
+    toh(e,'A','B','C');
     getch();
     return(0);
 }
 void toh (int n,char Beg, char Aux, char End )
 {
     if (n>=1){
+	
         toh(n-1,Beg,End,Aux);
-        printf ("%d Disk move %c to %c\n"n,Beg,End );
+        printf ("%d Disk move %c to %c\n",n,Beg,End );
         toh(n-1,Aux,Beg,End);
     }
+}
+    
